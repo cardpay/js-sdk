9c8036fd7e68ddcd10abf3fb692b3feb471ecba4
The name is hidden <>
2024-06-04 15:14:47 +0300

    chore: release 3.3.0

--- dist/types/data/pay-form.data.d.ts
+++ dist/types/data/pay-form.data.d.ts
@@ -1,12 +1,25 @@
 import { BillingAddress, Customer, MerchantOrder, PaymentData, ReturnUrls, Settings, ShippingAddress } from './common.data';
 type PayFormCustomer = Omit<Customer, 'phone'> & {
     locale?: string;
+    id?: string;
+    fullName?: string;
+    phone?: string;
+};
+type InstallmentItem = {
+    quantity: number;
+    amount: number;
+};
+type PayFormPaymentData = PaymentData & {
+    installmentType?: 'MF_HOLD' | 'IF';
+    installments?: InstallmentItem[];
+    holdPeriod?: number;
+    postAuthStatus?: 'Reverse' | 'Complete';
 };
 export type PayFormData = {
     merchantOrder?: MerchantOrder;
     shippingAddress?: ShippingAddress;
     paymentMethod: string;
-    paymentData: PaymentData;
+    paymentData: PayFormPaymentData;
     billingAddress: BillingAddress;
     customer: PayFormCustomer;
     returnUrls?: ReturnUrls;
